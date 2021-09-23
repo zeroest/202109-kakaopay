84b924ee64282b20fbe6a67e76fa281bfe5e2458
zeroest <husheart@naver.com>
2021-09-24 03:38:10 +0900

    Test code
    - ProductController allProduct test
    - ProductRepositoryTest split test

--- core/src/test/java/me/zeroest/kyd_kakaopay/repository/product/ProductRepositoryTest.java
+++ core/src/test/java/me/zeroest/kyd_kakaopay/repository/product/ProductRepositoryTest.java
@@ -2,6 +2,7 @@ package me.zeroest.kyd_kakaopay.repository.product;
 
 import com.querydsl.core.QueryResults;
 import me.zeroest.kyd_kakaopay.domain.product.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +21,8 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
-    @DisplayName("findProductAll 은 startedAt finishedAt 사이에 존재하는 Product 를 페이징하여 반환한다.")
-    @Test
-    void getAllProduct() {
+    @BeforeEach
+    void beforeEach() {
 
         List<Product> products = new ArrayList<>();
 
@@ -52,22 +52,39 @@ class ProductRepositoryTest {
 
         productRepository.saveAll(products);
 
+    }
+
+    @DisplayName("findProductAll 은 Product 를 페이징하여 반환한다.")
+    @Test
+    void getAllProductPaging() {
+
+        final LocalDateTime now2 = LocalDateTime.of(2021, 9, 21, 14, 2);
+        final QueryResults<Product> productAll2 = productRepository.findProductAll(
+                now2,
+                PageRequest.of(0, 1)
+        );
+        final List<Product> allProduct2 = productAll2.getResults();
+        assertEquals(2, productAll2.getTotal());
+        assertEquals(1, allProduct2.size());
+        assertEquals("product3", allProduct2.get(0).getTitle());
+
+    }
+
+    @DisplayName("findProductAll 은 startedAt finishedAt 사이에 존재하는 Product 를 반환한다.")
+    @Test
+    void getAllProduct() {
+
+        final LocalDateTime now1 = LocalDateTime.of(2021, 9, 21, 14, 0);
 
         final QueryResults<Product> productAll1 = productRepository.findProductAll(
-                LocalDateTime.of(2021, 9, 21, 14, 0),
+                now1,
                 PageRequest.of(0, 10)
         );
         final List<Product> allProduct1 = productAll1.getResults();
         assertEquals(1, productAll1.getTotal());
         assertEquals("product1", allProduct1.get(0).getTitle());
-
-        final QueryResults<Product> productAll2 = productRepository.findProductAll(
-                LocalDateTime.of(2021, 9, 21, 14, 2),
-                PageRequest.of(0, 10)
-        );
-        final List<Product> allProduct2 = productAll2.getResults();
-        assertEquals(2, productAll2.getTotal());
-        assertEquals("product3", allProduct2.get(0).getTitle());
+        assertTrue(allProduct1.get(0).getStartedAt().isBefore(now1));
+        assertTrue(allProduct1.get(0).getFinishedAt().isAfter(now1));
 
     }
 
---
+++ http_read/src/test/java/me/zeroest/kyd_kakaopay/controller/ProductControllerTest.java
@@ -0,0 +1,90 @@
+package me.zeroest.kyd_kakaopay.controller;
+
+import me.zeroest.kyd_kakaopay.domain.product.InvestStatus;
+import me.zeroest.kyd_kakaopay.domain.product.Product;
+import me.zeroest.kyd_kakaopay.dto.product.ProductDto;
+import me.zeroest.kyd_kakaopay.dto.response.PageResponse;
+import me.zeroest.kyd_kakaopay.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ProductService productService;
+
+    private PageResponse<ProductDto> expected;
+
+    @BeforeEach
+    void beforeEach() {
+        List<ProductDto> productDtoList = Arrays.asList(
+                new ProductDto(
+                        Product.builder()
+                                .id(1L)
+                                .title("product1")
+                                .totalAmount(50L)
+                                .startedAt(LocalDateTime.now())
+                                .finishedAt(LocalDateTime.now())
+                                .build()
+                        , 10L
+                        , 10L
+                ),
+                new ProductDto(
+                        Product.builder()
+                                .id(2L)
+                                .title("product1")
+                                .totalAmount(50L)
+                                .startedAt(LocalDateTime.now())
+                                .finishedAt(LocalDateTime.now())
+                                .build()
+                        , 50L
+                        , 10L
+                )
+        );
+
+        expected = new PageResponse(productDtoList, (long) productDtoList.size());
+    }
+
+    @DisplayName("GET /product 를 통해 Product 리스트를 받아온다.")
+    @Test
+    void allProduct() throws Exception {
+
+        when(productService.getAllProduct(any(LocalDateTime.class), any(Pageable.class)))
+                .thenReturn(expected);
+
+        int page = 0;
+        int size = 10;
+
+        mvc.perform(MockMvcRequestBuilders.get("/product?page="+page+"&size="+size))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result").value(true))
+                .andExpect(jsonPath("$.data.totalCount").value(expected.getTotalCount()))
+                .andExpect(jsonPath("$.data.list[0].productId").value(1L))
+                .andExpect(jsonPath("$.data.list[0].investStatus").value(InvestStatus.ONGOING.toString()))
+                .andExpect(jsonPath("$.data.list[1].productId").value(2L))
+                .andExpect(jsonPath("$.data.list[1].investStatus").value(InvestStatus.COMPLETE.toString()));
+
+    }
+
+}
